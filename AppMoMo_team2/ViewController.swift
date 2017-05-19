6b18cc4859df9e176abb4bf3fc2bb4134eb4bd9b
cuscedu <cuscedu@mac06.local>
2017-05-19 16:37:42 +0700

    Initial Commit

---
+++ AppMoMo_team2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppMoMo_team2
+//
+//  Created by cuscedu on 5/19/17.
+//  Copyright © 2017 cuscedu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
