730eccf67dd70628c5b70d7cbb96f8a43be6e689
cuscedu <cuscedu@mac05.local>
2017-05-19 16:59:45 +0700

    commit trung

--- AppMoMo_team2/ViewController.swift
+++ AppMoMo_team2/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-//abc ahihi
+//abc ahihi ahihi
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
